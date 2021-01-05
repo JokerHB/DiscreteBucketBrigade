10e8cf27b89628cfe72f8009716471d67c614c98
JokerHB <aifjhb0815@gmail.com>
2021-01-05 19:49:37 +0800

    -fix bug on read/write file method

--- DiscreteBucketBrigade/Source/Simulation/Simulation.cpp
+++ DiscreteBucketBrigade/Source/Simulation/Simulation.cpp
@@ -34,6 +34,8 @@ std::vector<std::string> split(const std::string &str, const std::string &delim)
 
 Simulation::Simulation()
 {
+    this->stationPath = "./station.txt";
+    this->workerPath = "./worker.txt";
 }
 
 Simulation::~Simulation()
@@ -73,11 +75,32 @@ bool Simulation::GetFullorPartial()
 void Simulation::GenerateStations()
 {
     // TODO Generate Stations
+    Station station0 = Station(0, Idle, 1.0);
+    Station station1 = Station(1, Idle, 1.0);
+    Station station2 = Station(2, Idle, 1.0);
+    Station station3 = Station(3, Idle, 1.0);
+    Station station4 = Station(4, Idle, 1.0);
+
+    this->stations.push_back(station0);
+    this->stations.push_back(station1);
+    this->stations.push_back(station2);
+    this->stations.push_back(station3);
+    this->stations.push_back(station4);
 }
 
 void Simulation::GenerateWorkers()
 {
     // TODO Generate Workers
+    double v0 = 0.5;
+    double v1 = 1.0;
+    double v2 = 1.5;
+    Worker worker0 = Worker(0, Idle, v0, 2, std::vector<int>({0, 1, 2}));
+    Worker worker1 = Worker(1, Idle, v1, 3, std::vector<int>({2, 3}));
+    Worker worker2 = Worker(2, Idle, v2, 4, std::vector<int>({3, 4}));
+
+    this->workers.push_back(worker0);
+    this->workers.push_back(worker1);
+    this->workers.push_back(worker2);
 }
 
 std::vector<Station> Simulation::GetStations()
@@ -160,6 +183,8 @@ void Simulation::StationReadFromFile(std::string filePath)
     {
         std::string info;
         std::getline(reader, info);
+        if (info == "")
+            continue;
         std::vector<std::string> infos = split(info, "\t");
 
         if (infos.size() != 3)
@@ -185,7 +210,7 @@ void Simulation::WorkerReadFromFile(std::string filePath)
     if (!reader.is_open())
     {
         std::cout << "Error, can not read " << filePath << std::endl;
-        exit(-10);
+        exit(-12);
     }
 
     std::string tmp;
@@ -194,12 +219,14 @@ void Simulation::WorkerReadFromFile(std::string filePath)
     {
         std::string info;
         std::getline(reader, info);
+        if (info == "")
+            continue;
         std::vector<std::string> infos = split(info, "\t");
 
         if (infos.size() < 5)
         {
             std::cout << "Error, file " << filePath << " error" << std::endl;
-            exit(-12);
+            exit(-13);
         }
 
         int id = std::stoi(infos[0]);
--- DiscreteBucketBrigade/main.cpp
+++ DiscreteBucketBrigade/main.cpp
@@ -18,32 +18,51 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-    vector<Station> stations;
-    vector<Worker> workers;
     Simulation simulation = Simulation();
+
+    simulation.GenerateStations();
+    simulation.GenerateWorkers();
+
+    simulation.StationWriteToFile();
+    simulation.WorkerWriteToFile();
+
+    // simulation.StationReadFromFile("./station.txt");
+    // simulation.WorkerReadFromFile("./worker.txt");
+
+    vector<Station> stations = simulation.GetStations();
+    vector<Worker> workers = simulation.GetWorkers();
 
     double v0 = 0.5;
     double v1 = 1.0;
     double v2 = 1.5;
 
-    Worker worker0 = Worker(0, Idle, v0, 2, vector<int>({0, 1, 2}));
-    Worker worker1 = Worker(1, Idle, v1, 3, vector<int>({2, 3}));
-    Worker worker2 = Worker(2, Idle, v2, 4, vector<int>({3, 4}));
-    Station station0 = Station(0, Idle, 1.0);
-    Station station1 = Station(1, Idle, 1.0);
-    Station station2 = Station(2, Idle, 1.0);
-    Station station3 = Station(3, Idle, 1.0);
-    Station station4 = Station(4, Idle, 1.0);
+    // Worker worker0 = Worker(0, Idle, v0, 2, vector<int>({0, 1, 2}));
+    // Worker worker1 = Worker(1, Idle, v1, 3, vector<int>({2, 3}));
+    // Worker worker2 = Worker(2, Idle, v2, 4, vector<int>({3, 4}));
+    // Station station0 = Station(0, Idle, 1.0);
+    // Station station1 = Station(1, Idle, 1.0);
+    // Station station2 = Station(2, Idle, 1.0);
+    // Station station3 = Station(3, Idle, 1.0);
+    // Station station4 = Station(4, Idle, 1.0);
 
-    stations.push_back(station0);
-    stations.push_back(station1);
-    stations.push_back(station2);
-    stations.push_back(station3);
-    stations.push_back(station4);
-    workers.push_back(worker0);
-    workers.push_back(worker1);
-    workers.push_back(worker2);
+    // stations.push_back(station0);
+    // stations.push_back(station1);
+    // stations.push_back(station2);
+    // stations.push_back(station3);
+    // stations.push_back(station4);
+    // workers.push_back(worker0);
+    // workers.push_back(worker1);
+    // workers.push_back(worker2);
 
+    for (int i = 0; i < stations.size(); i++)
+    {
+        cout << stations[i].ToString() << endl;
+    }
+
+    for (int i = 0; i < workers.size(); i++)
+    {
+        cout << workers[i].ToString() << endl;
+    }
 
     ProductionLine productionLine = ProductionLine(stations, workers, 10000);
     double throughput = productionLine.Run();
