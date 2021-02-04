c51ec5b10dd7c114c1c49afe7a7f077d55e7f933
JokerHB <aifjhb0815@gmail.com>
2021-02-04 19:31:33 +0800

    -add chaos

---
+++ DiscreteBucketBrigade/Chaos.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from math import ceil
+
+
+def getHandoff(pos, speed):
+    ret = [0.0]
+    ret.append(ceil(((10.0 - pos[2]) / speed[2]) * speed[0]))
+    ret.append(pos[1] + ceil(((10.0 - pos[2]) / speed[2]) * speed[1]))
+
+    for i in range(1, len(ret)):
+        ret[i] = ret[i] if ret[i] >= 1.0 else 1.0
+        ret[i] = 9.0 if ret[i] >= 9.0 else ret[i]
+
+    return ret
+
+
+if __name__ == '__main__':
+    pos = [1.0, 2.0, 3.0]
+    speed = [0.8, 0.5, 0.9]
+    cnt = 0
+
+    while cnt < 1000:
+        print(pos)
+        pos = getHandoff(pos=pos, speed=speed)
+        cnt += 1
